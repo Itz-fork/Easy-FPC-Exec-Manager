deb10b57834e5301179088f31352e38dd6ff9b95
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-09-26 14:07:01 +0530

    Say Hi to Pascal Executor Script

---
+++ script/linux/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+pascal_code="./*.pas"
+
+
+_compile() {
+    for eachfile in $pascal_code
+    do
+        file_name=${eachfile//[.pas]/""}
+        fpc "$eachfile"
+        "./$file_name"
+    done
+}
+
+_run() {
+    printf "Output: \n\n"
+    "./$file_name"
+    printf "\n"
+}
+
+_rm_compiled_files() {
+    r_fn=${eachfile/.pas/""}
+    rm "$r_fn"
+    rm "$r_fn.o"
+}
+
+_main(){
+    printf "\n Copyright (c) 2021 Itz-fork \n\n"
+    echo "----> Compiling Your Pascal Program(s)"
+    _compile &> /dev/null
+    echo "----> Running Your Pascal Program(s)"
+    _run
+    _rm_compiled_files &> /dev/null
+}
+_main
