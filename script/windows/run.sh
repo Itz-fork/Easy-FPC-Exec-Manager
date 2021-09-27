4f330d2af6133d1dafb89cb9112fa7fa2aa62d4a
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-09-27 18:59:12 +0530

    Fixed the windows version

--- script/windows/run.sh
+++ script/windows/run.sh
@@ -9,22 +9,20 @@ _add_doskey() {
 _compile() {
     for eachfile in $pascal_code
     do
-        file_name=${eachfile//[.pas]/""}
         fpc "$eachfile"
-        "./$file_name"
     done
 }
 
 _run() {
+    file_name=${eachfile/.pas/""}
     printf "Output: \n\n"
     "./$file_name"
     printf "\n"
 }
 
 _rm_compiled_files() {
-    r_fn=${eachfile/.pas/""}
-    del /f "$r_fn" || rm "$r_fn"
-    del /f "$r_fn.o" || rm "$r_fn.o"
+    del /f "$file_name" || rm "$file_name"
+    del /f "$file_name.o" || rm "$file_name.o"
 }
 
 _main(){
