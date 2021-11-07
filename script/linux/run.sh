a155a67f7aa3fe85951f27809528aef96324c118
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-11-07 18:24:22 +0530

    Improvements (linux version)
    - Added support for executing multiple programs at once
    - Better outputs

--- script/linux/run.sh
+++ script/linux/run.sh
@@ -1,33 +1,40 @@
 #!/usr/bin/bash
 
+
+# Compile the pascal program
 pascal_code="./*.pas"
-
-
 _compile() {
+    p_no=1
     for eachfile in $pascal_code
     do
-        fpc "$eachfile"
+        fpc "$eachfile" &> /dev/null
+        _run
+        p_no=$((p_no+1))
+        _rm_compiled_files
     done
 }
 
+# Run the compiled programs
 _run() {
     file_name=${eachfile/.pas/""}
-    printf "Output: \n\n"
-    "./$file_name"
-    printf "\n"
+    echo -e "Program $p_no Output: \n\n"
+    "$file_name"
+    echo -e "\n\n_________________ FINISHED _________________ \n\n"
 }
 
+# Remove compiled files
 _rm_compiled_files() {
     rm "$file_name"
     rm "$file_name.o"
 }
 
+# Main function
 _main(){
+    clear
     printf "\n Copyright (c) 2021 Itz-fork \n\n"
     echo "----> Compiling Your Pascal Program(s)"
-    _compile &> /dev/null
-    echo "----> Running Your Pascal Program(s)"
-    _run
+    echo "----> Soon you will see the output(s)"
+    _compile
     _rm_compiled_files &> /dev/null
 }
 _main
